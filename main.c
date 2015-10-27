270efd5677242a0b9430e9a28db683d89d7d229f
Ahmed Gaber <ahmedgaber20@gmail.com>
2015-10-27 22:14:51 +0200

    Implement line-parser

--- main.c
+++ main.c
@@ -13,10 +13,11 @@ int b_dimensions[2];
 int c_dimensions[2];
 
 void run();
-int read_matrix_from_file(char *file_name);
+int read_matrix_from_file(char *file_name, int mat_num);
 int write_matrix_to_file(char *file_name);
+char **get_files_list();
 char *read_line();
-char* concat(char *s1, char *s2);
+char* connect_strings(char *s1, char *s2);
 char **parse_line(char *line);
 
 int main()
@@ -28,26 +29,62 @@ int main()
 
 void run()
 {
-    char *line = read_line();
-
+  char **files = get_files_list();
 }
 
-int read_matrix_from_file(char *file_name)
+/**
+  Reads a matrix from file
+*/
+int read_matrix_from_file(char *file_name, int mat_num)
 {
 
 }
 
+/**
+  Writes a matrix to file
+*/
 int write_matrix_to_file(char *file_name)
 {
 
 }
 
-char *read_line()
-{
+/**
+  Reads file names from user, and return all in and out file names
+*/
+char **get_files_list(){
+  char *line = read_line();
+  char **files = parse_line(line);
 
+  if (strcmp(files[0], "") == 0){
+    files[1] = "a.txt";
+    files[2] = "b.txt";
+    files[3] = "c.out";
+  }
+  else if (strcmp(files[1], "") == 0){
+    files[1] = connect_strings(files[0], "a.txt");
+    files[2] = connect_strings(files[0], "b.txt");
+    files[3] = connect_strings(files[0], "c.out");
+  }
+  else{
+    files[1] = connect_strings(files[0], files[1]);
+    files[2] = connect_strings(files[0], files[2]);
+    files[3] = connect_strings(files[0], files[3]);
+  }
+  return files;
 }
 
-char *concat(char *s1, char *s2)
+/**
+  Reads a line from terminal
+*/
+char *read_line()
+{
+  char *line = malloc(sizeof(char) * 200);
+}
+
+/**
+  connects two strings
+*/
+char *connect_strings(char *s1, char *s2)
 {
     char *result = malloc(strlen(s1)+strlen(s2)+1);
     strcpy(result, s1);
@@ -55,6 +92,9 @@ char *concat(char *s1, char *s2)
     return result;
 }
 
+/**
+  parses a line and returns the parsed values in a 2D array
+*/
 char **parse_line(char *line)
 {
     int current = 0;
